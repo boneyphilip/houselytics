2a62d5cb5c310c937d2ca7ecd318081824086ace
boneyphilip <boneyphilip@ymail.com>
2026-03-02 23:08:08 +0100

    Add Heroku deployment configuration

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: streamlit run app.py --server.address 0.0.0.0 --server.port $PORT
+web: sh setup.sh && streamlit run app.py
---
+++ setup.sh
@@ -0,0 +1,8 @@
+mkdir -p ~/.streamlit/
+
+cat > ~/.streamlit/config.toml << EOF
+[server]
+headless = true
+port = ${PORT}
+enableCORS = false
+EOF
